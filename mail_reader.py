7a4722125cd4668e4bcb17296d44f2ab654863d2
kuzyivan <kuzmenkoivan1990@gmail.com>
2025-04-28 16:38:35 +1000

    Update mail_reader.py

--- mail_reader.py
+++ mail_reader.py
@@ -1,2 +1,8 @@
-EMAIL = 'bottrack@yandex.ru'
-PASSWORD = 'hsjbtrebufwtsdfa'
+from apscheduler.schedulers.background import BackgroundScheduler
+
+def start_mail_checking():
+    init_db()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(check_mail, 'interval', minutes=40)
+    scheduler.start()
+    print('🔄 Фоновая проверка почты запущена.')
