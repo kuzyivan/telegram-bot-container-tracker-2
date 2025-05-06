28670bec851f2d8aee7475bfeb7af46b48e87cf3
kuzyivan <kuzmenkoivan1990@gmail.com>
2025-05-06 12:11:44 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,8 @@ import logging
 import re
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-from apscheduler.schedulers.background import BackgroundScheduler
-from mail_reader import ensure_database_exists, schedule_mail_checking
-from backup_db import schedule_backup
-
+from mail_reader import start_mail_checking
+from backup_db import start_backup_scheduler
 
 # Логирование
 logging.basicConfig(level=logging.INFO)
@@ -86,18 +84,13 @@ async def find_container(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Инициализация бота
 if __name__ == "__main__":
-    ensure_database_exists()
-
-    scheduler = BackgroundScheduler()
-    schedule_mail_checking(scheduler)
-    schedule_backup(scheduler)
-    scheduler.start()
+    start_mail_checking()
+    start_backup_scheduler()
 
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, find_container))
     logger.info("✨ Бот запущен!")
-
     app.run_webhook(
         listen="0.0.0.0",
         port=PORT,
