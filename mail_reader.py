4433430569fdb57ae7f89f0bc1500bf3d4c85fea
kuzyivan <kuzmenkoivan1990@gmail.com>
2025-05-16 13:51:51 +1000

    Update mail_reader.py

--- mail_reader.py
+++ mail_reader.py
@@ -36,7 +36,7 @@ def ensure_database_exists():
     conn = get_pg_connection()
     cursor = conn.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS tracking (
-                        id SERIAL PRIMARY KEY,,
+                        id SERIAL PRIMARY KEY,
                         container_number TEXT,
                         from_station TEXT,
                         to_station TEXT,
