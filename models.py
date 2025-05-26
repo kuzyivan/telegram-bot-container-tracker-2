932f92b27cbe131f1d837a45d24b43e5ad6192b0
kuzyivan <kuzmenkoivan1990@gmail.com>
2025-05-26 21:30:28 +0000

    new file:   models.py

---
+++ models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, String, BigInteger, DateTime, Float
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+class Stat(Base):
+    __tablename__ = 'stats'
+
+    id = Column(Integer, primary_key=True)
+    container_number = Column(String)
+    user_id = Column(BigInteger)
+    username = Column(String)
+    timestamp = Column(DateTime, default=func.now())
+
+class Tracking(Base):
+    __tablename__ = 'tracking'
+
+    id = Column(Integer, primary_key=True)
+    container_number = Column(String)
+    from_station = Column(String)
+    to_station = Column(String)
+    current_station = Column(String)
+    operation = Column(String)
+    operation_date = Column(String)
+    waybill = Column(String)
+    km_left = Column(Integer)
+    forecast_days = Column(Float)
+    wagon_number = Column(String)
+    operation_road = Column(String)
