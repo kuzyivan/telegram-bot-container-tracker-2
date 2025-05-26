ba4b4e439bb027e373c159008c7255bc84a64ff2
kuzyivan <kuzmenkoivan1990@gmail.com>
2025-05-26 23:09:37 +1000

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,14 @@
 import os
-import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
 
-def get_pg_connection():
-    return psycopg2.connect(
-        host=os.getenv("POSTGRES_HOST"),
-        port=os.getenv("POSTGRES_PORT", 5432),
-        dbname=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD")
-    )
+def get_pg_engine() -> Engine:
+    user = os.getenv("POSTGRES_USER")
+    password = os.getenv("POSTGRES_PASSWORD")
+    host = os.getenv("POSTGRES_HOST")
+    port = os.getenv("POSTGRES_PORT", "5432")
+    dbname = os.getenv("POSTGRES_DB")
+
+    url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}"
+    engine = create_engine(url)
+    return engine
