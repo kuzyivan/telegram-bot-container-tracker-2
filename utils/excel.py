f7960837088307193e79ffa07d512a9335491fe8
kuzyivan <kuzmenkoivan1990@gmail.com>
2025-05-30 08:33:46 +0000

    datetime.now(timezone.utc

--- utils/excel.py
+++ utils/excel.py
@@ -1,10 +1,17 @@
+
 import pandas as pd
+import os
 import tempfile
+from datetime import datetime
 from openpyxl.styles import PatternFill
+from datetime import datetime, timezone
 
 def generate_dislocation_excel(df: pd.DataFrame) -> str:
-    tmp = tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx")
-    with pd.ExcelWriter(tmp.name, engine='openpyxl') as writer:
+    filename = f"dislocation_{datetime.now(timezone.utc).strftime('%Y%m%d_%H%M%S')}.xlsx"
+    temp_dir = tempfile.gettempdir()
+    path = os.path.join(temp_dir, filename)
+
+    with pd.ExcelWriter(path, engine='openpyxl') as writer:
         df.to_excel(writer, index=False, sheet_name='Дислокация')
         worksheet = writer.sheets['Дислокация']
         fill = PatternFill(start_color='87CEEB', end_color='87CEEB', fill_type='solid')
@@ -13,4 +20,6 @@ def generate_dislocation_excel(df: pd.DataFrame) -> str:
         for col in worksheet.columns:
             max_length = max(len(str(cell.value)) if cell.value else 0 for cell in col)
             worksheet.column_dimensions[col[0].column_letter].width = max_length + 2
-    return tmp.name
+
+    return path
+
