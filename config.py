d8767bec58da262e07b74bf5210b6ba46d8ad242
kuzyivan <kuzmenkoivan1990@gmail.com>
2025-07-02 07:28:36 +1000

    vds

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID")) # type: ignore
 RENDER_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
 PORT = int(os.environ.get("PORT", 10000))
