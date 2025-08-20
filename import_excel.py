31dab2bc5cb25c5f58e703fcc409aa9dbbb306eb
kuzyivan <kuzmenkoivan1990@gmail.com>
2025-08-20 16:29:45 +1000

    import_excel.py

--- import_excel.py
+++ import_excel.py
@@ -1,12 +1,16 @@
-from services.container_importer import import_loaded_and_dispatch_from_excel
 import asyncio
 from pathlib import Path
-from datetime import datetime
+from services.container_importer import import_loaded_and_dispatch_from_excel
+from logger import get_logger
+
+logger = get_logger(__name__)
 
 async def main():
-    today = datetime.now().strftime("%d.%m.%Y")
-    filepath = Path(f"/root/AtermTrackBot/A-Terminal {today}.xlsx")
-    await import_loaded_and_dispatch_from_excel(str(filepath))
+    today_file = Path("/root/AtermTrackBot/A-Terminal 20.08.2025.xlsx")  # Укажи нужную дату, если файл отличается
+    if today_file.exists():
+        await import_loaded_and_dispatch_from_excel(str(today_file))
+    else:
+        logger.error(f"❌ Файл не найден: {today_file}")
 
 if __name__ == "__main__":
     asyncio.run(main())
