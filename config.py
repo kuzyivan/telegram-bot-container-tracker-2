189770ecc1876462291cfeefbff5fec56f198e91
kuzyivan <kuzmenkoivan1990@gmail.com>
2025-08-21 22:25:45 +1000

    config.py

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ TOKEN = os.getenv("TELEGRAM_TOKEN")
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID")) # type: ignore
 RENDER_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
 PORT = int(os.environ.get("PORT", 10000))
+#стабильная версия
